adeafb13616cc74e696247a09155809d89117da4
Brian Yingling <brian@jet.com>
2015-10-18 15:15:36 -0400

    stubbed out register page

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,11 @@ module.exports = function(config) {
   // home
   var app = config.app;
   var home = config.controllers.home;
+  var register = config.controllers.register;
 
   app.get('/', home.index);
   app.get('/home', home.index);
+
+  app.get('/register/new', register.new);
+
 }
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var path = require('path');
 var app = express();
 
 // controllers
-var home = require('./controllers/home');
+var controllers = {
+  home: require('./controllers/home'),
+  register: require('./controllers/register')
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -11,9 +14,7 @@ app.use('/static', express.static(__dirname + '/public'));
 
 var config = {
   app: app,
-  controllers: {
-    home: home
-  }
+  controllers: controllers
 }
 
 // routes
